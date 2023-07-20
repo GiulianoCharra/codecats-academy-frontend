9a26d87a4381e831645b1b47109c37bc39e03fd5
GiulianoCharra <65138966+GiulianoCharra@users.noreply.github.com>
2023-07-20 02:52:38 -0300

    agregado carrucel, falta completar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
-const container = document.querySelector(".container");
-const cardsWrapper = document.querySelector(".cards-wrapper");
+// const contenedorListado = document.querySelector(".contenedor-listado");
+// const listadoIntegrantes = document.querySelector(".listado_integrantes");
 
-container.addEventListener("scroll", () => {
-  if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
-    // Llegaste al final, mueve la primera card al final del carrusel
-    cardsWrapper.appendChild(cardsWrapper.firstElementChild);
-  }
+// listadoIntegrantes.addEventListener("scroll", () => {
+//   if (
+//     listadoIntegrantes.scrollLeft + listadoIntegrantes.clientWidth >=
+//     listadoIntegrantes.scrollWidth
+//   ) {
+//     // Llegaste al final, mueve la primera card al final del carrusel
+//     listadoIntegrantes.appendChild(listadoIntegrantes.firstElementChild);
+//   }
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Obtenemos el contenedor del carrusel
+  const carouselContainer = document.querySelector(".contenedor-listado");
+
+  // Función para desplazar el carrusel hacia la izquierda
+  const moveCarousel = () => {
+    const carousel = carouselContainer.querySelector(".listado_integrantes");
+    const firstCard = carousel.querySelector(".integrante");
+    carousel.appendChild(firstCard);
+  };
+
+  // Definimos el intervalo para mover el carrusel cada 5 segundos (ajústalo según tus necesidades)
+  setInterval(moveCarousel, 5000);
 });
