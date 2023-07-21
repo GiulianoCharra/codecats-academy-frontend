0f85c88b2b6208905e81a9af1b5884463e41f197
GiulianoCharra <65138966+GiulianoCharra@users.noreply.github.com>
2023-07-21 00:51:03 -0300

    optimizado la carga imagenes y las imagenes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # codecats-academy
-CodeCats Academy es una pagina web de cursos digitales donde cualquier persona interesada pueda realizar cursos virtuales relacionados a la programacion con una tematica gatuna
+CodeCats Academy es una pagina web de cursos digitales donde cualquier persona interesada pueda realizar cursos virtuales relacionados a la programación con una temática gatuna
+
+
+Diseño realizado en Figma
+[enlace](https://www.figma.com/file/zRP0YVd9PcDjgJvhrRR0Ca/CodeCats-Academy?type=design&t=P7e4vcjmB2r5kfga-6)
+
+Tecnologías y metodologías usadas:
+
+    - Javascript
+    - HTML
+    - CSS
+    - Desktop First
+    - BEM CSS
+
--- js/script.js
+++ js/script.js
@@ -1,27 +1,65 @@
-// const contenedorListado = document.querySelector(".contenedor-listado");
-// const listadoIntegrantes = document.querySelector(".listado_integrantes");
+const listadoIntegrantes = document.querySelector(".listado_integrantes");
 
-// listadoIntegrantes.addEventListener("scroll", () => {
-//   if (
-//     listadoIntegrantes.scrollLeft + listadoIntegrantes.clientWidth >=
-//     listadoIntegrantes.scrollWidth
-//   ) {
-//     // Llegaste al final, mueve la primera card al final del carrusel
-//     listadoIntegrantes.appendChild(listadoIntegrantes.firstElementChild);
-//   }
-// });
-
-document.addEventListener("DOMContentLoaded", () => {
+/* document.addEventListener("DOMContentLoaded", () => {
   // Obtenemos el contenedor del carrusel
   const carouselContainer = document.querySelector(".contenedor-listado");
-
   // Función para desplazar el carrusel hacia la izquierda
   const moveCarousel = () => {
     const carousel = carouselContainer.querySelector(".listado_integrantes");
     const firstCard = carousel.querySelector(".integrante");
     carousel.appendChild(firstCard);
   };
-
   // Definimos el intervalo para mover el carrusel cada 5 segundos (ajústalo según tus necesidades)
   setInterval(moveCarousel, 5000);
-});
+}); */
+
+// Función para desplazar automáticamente el carrusel
+
+/* function autoScroll() {
+  listadoIntegrantes.scrollBy({ left: 300, behavior: "smooth" });
+}
+setInterval(autoScroll, 5000); */
+
+// Función para detectar la tarjeta en el centro y aplicar la clase del hover
+function detectarCentro() {
+  // Obtener el ancho del contenedor y la posición del scroll horizontal
+  const contenedorAncho = listadoIntegrantes.clientWidth;
+  const scrollPosicion = listadoIntegrantes.scrollLeft;
+
+  // Obtener las tarjetas y su ancho total
+  const tarjetas = listadoIntegrantes.querySelectorAll(".integrante");
+  const tarjetasAnchoTotal = Array.from(tarjetas).reduce(
+    (total, tarjeta) => total + tarjeta.clientWidth,
+    0
+  );
+
+  // Calcular la posición del centro en el carrusel
+  const centroPosicion = scrollPosicion + contenedorAncho / 2;
+
+  // Encontrar la tarjeta que está en el centro
+  let tarjetaCentro = null;
+  let acumuladoAncho = 0;
+  for (const tarjeta of tarjetas) {
+    acumuladoAncho += tarjeta.clientWidth + 10; // 10 es el margen derecho entre tarjetas
+    if (acumuladoAncho >= centroPosicion) {
+      tarjetaCentro = tarjeta;
+      break;
+    }
+  }
+
+  // Remover la clase del hover de todas las tarjetas
+  tarjetas.forEach((tarjeta) => {
+    tarjeta.classList.remove("hover-activo");
+  });
+
+  // Agregar la clase del hover a la tarjeta del centro
+  if (tarjetaCentro) {
+    tarjetaCentro.classList.add("hover-activo");
+  }
+}
+
+// Detectar el centro al hacer scroll en el carrusel
+listadoIntegrantes.addEventListener("scroll", detectarCentro);
+
+// Detectar el centro al cargar la página
+detectarCentro();
